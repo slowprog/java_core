4298f0345f2670e6dfb531630ebf664cc244ccc6
Andrey Tyshev <slowprog.job@gmail.com>
2017-09-05 00:21:46 +0300

    Add 4th lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        // MyWindow myWindow = new MyWindow();
+        TestWindow testWindow= new TestWindow();
     }
 }
